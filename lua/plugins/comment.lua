4e4d41cbeac2b54ee20b34f94d529e112f92d730
tejasmk-tkp <dawn3tkp@gmail.com>
2025-09-17 10:07:12 +0530

    enable block comments for python

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -2,6 +2,15 @@ return {
   "numToStr/Comment.nvim",
   event = { "BufReadPre", "BufNewFile" },
   config = function()
-    require("Comment").setup()
+    require("Comment").setup({
+      -- Custom commentstring for different filetypes
+      pre_hook = function(ctx)
+        -- For Python, always use line comments even in block mode
+        if vim.bo.filetype == 'python' then
+          -- Force line comments for Python in both line and block modes
+          return require('Comment.ft').get('python', ctx.ctype == require('Comment.utils').ctype.linewise and 1 or 1)
+        end
+      end,
+    })
   end,
 }
