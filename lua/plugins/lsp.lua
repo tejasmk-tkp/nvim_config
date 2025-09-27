a8cdd2784de9fc4ab022a128bc75cfe37ea59d81
tejasmk-tkp <dawn3tkp@gmail.com>
2025-09-27 21:35:08 +0530

    remove deprecated

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -51,7 +51,7 @@ return {
 		"neovim/nvim-lspconfig",
 		lazy = false,
 		config = function()
-			local lspconfig = require("lspconfig")
+			local lspconfig = vim.lsp.config
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			capabilities.offsetEncoding = { "utf-8" }
 			
