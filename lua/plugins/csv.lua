2efae1c82792db110e0479b6cc207d7c3c884adf
tejasmk-tkp <dawn3tkp@gmail.com>
2026-02-12 00:57:01 +0530

    csv viewer

---
+++ lua/plugins/csv.lua
@@ -0,0 +1,19 @@
+return {
+    "hat0uma/csvview.nvim",
+    ft = { "csv" },
+    config = function()
+        require("csvview").setup({
+            view = {
+                display_mode = "border", -- "border" or "highlight"
+            },
+        })
+
+        -- Auto-enable csvview when opening CSV files
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = "csv",
+            callback = function()
+                vim.cmd("CsvViewEnable")
+            end,
+        })
+    end,
+}
